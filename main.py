403fef7418a0fb6346f537a294014507b874ee35
Enliden <mmc_git@your-mail.com>
2025-07-22 13:06:30 +0200

    moved count_word function to new file stats.py

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
+from stats import count_words
+
 def get_book_text(filepath):
     with open(filepath) as file:
         contents = file.read()
         return contents
-
-def count_words(content_of_book):
-    word_list = content_of_book.split()
-    return len(word_list)
 
 def main():
     contents = get_book_text("books/frankenstein.txt") 
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words(content_of_book):
+    word_list = content_of_book.split()
+    return len(word_list)
