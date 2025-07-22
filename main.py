bbc5aa68f51f0288be0f1f014d6927d2f157c17f
Enliden <mmc_git@your-mail.com>
2025-07-22 14:18:04 +0200

    added character count, proper reporting and filepath argument

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-from stats import count_words
+from stats import count_words, count_characters, sort_char_dict
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as file:
         contents = file.read()
         return contents
 
-def main():
-    contents = get_book_text("books/frankenstein.txt") 
+def main(filepath):
+    print("============ BOOKBOT ============")
+    
+    contents = get_book_text(filepath) 
+    print(f"Analyzing book found at {filepath}...")
+    
+    print("----------- Word Count ----------")
     number_of_words = count_words(contents)
-    print(f"{number_of_words} words found in the document")
+    print(f"Found {number_of_words} total words")
+    
+    print("--------- Character Count -------")
+    char_count = count_characters(contents)
+    sorted_char_count = sort_char_dict(char_count)
+    for c in sorted_char_count:
+        if c["char"].isalpha():
+            print(f"{c["char"]}: {c["num"]}")
+    print("============= END ===============")
 
-main()
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+main(sys.argv[1])
--- stats.py
+++ stats.py
@@ -1,3 +1,27 @@
 def count_words(content_of_book):
     word_list = content_of_book.split()
     return len(word_list)
+
+def count_characters(content_of_book):
+    content_of_book = content_of_book.lower()
+    
+    num_of_chars = {}
+    
+    for c in content_of_book:
+        if c in num_of_chars:
+            num_of_chars[c] += 1
+        else:
+            num_of_chars[c] = 1    
+    return num_of_chars
+
+def sort_char_dict(num_of_chars):
+    list = []
+    for c in num_of_chars:
+        list.append({"char" : c, "num" : num_of_chars[c]})
+
+    def sort_on(dict):
+        return dict["num"]
+
+    list.sort(key=sort_on, reverse = True)
+    return list
+        
